a173c9670c53c59667ad1856ca661376209bf27c
Unknown <usfirepilot123@gmail.com>
2017-08-08 00:07:52 -0600

    Setup 1.0.2

--- src/main/java/com/fireboss/heartlevels/Reference.java
+++ src/main/java/com/fireboss/heartlevels/Reference.java
@@ -3,7 +3,7 @@ package com.fireboss.heartlevels;
 public class Reference {
 	public static final String MOD_ID = "heartlevels";
 	public static final String NAME = "Heart Levels";
-	public static final String VERSION = "1.0.0 BETA";
+	public static final String VERSION = "1.0.2";
 	public static final String MCVERSIONS = "[1.8.9]";
 	
 	public static final String CLIENT_PROXY = "com.fireboss.heartlevels.proxy.ClientProxy";
